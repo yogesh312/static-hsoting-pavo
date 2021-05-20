51292ce12b04c34553d3f587a4d3304b3d3ae910
yogesh <etpr.yogesh@gmail.com>
2021-05-20 16:02:28 +0530

    Updates

---
+++ precache-manifest.93733c203b691380fc0e5d7bc7faf158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ddb36546fd8edb87f1f002215a42676",
+    "url": "/static-hsoting-pavo/index.html"
+  },
+  {
+    "revision": "c50c4730ec04cf67edf5",
+    "url": "/static-hsoting-pavo/static/css/main.d8f5e53f.chunk.css"
+  },
+  {
+    "revision": "a1f4f216db3e2ef69a13",
+    "url": "/static-hsoting-pavo/static/js/2.295043b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static-hsoting-pavo/static/js/2.295043b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c50c4730ec04cf67edf5",
+    "url": "/static-hsoting-pavo/static/js/main.3e2c585e.chunk.js"
+  },
+  {
+    "revision": "0dc1307479438854bafc",
+    "url": "/static-hsoting-pavo/static/js/runtime-main.fcea7eb2.js"
+  }
+]);
